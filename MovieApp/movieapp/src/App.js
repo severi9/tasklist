caf5050122588b563050d584423bbc622b931e07
severi9 <severi.x@hotmail.com>
2024-11-11 13:27:28 +0200

    initial_commit

---
+++ MovieApp/movieapp/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="">
+      <header className="">
+        <h3>TESTI</h3>
+      </header>
+    </div>
+  );
+}
+
+export default App;
